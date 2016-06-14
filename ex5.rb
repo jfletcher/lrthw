758f413e3f7b9971e7765d1b2d3b7845369af3d5
Jamie Fletcher <jfletcher@craven-college.ac.uk>
2016-06-14 16:36:38 +0100

    Add exercise 5

---
+++ ex5.rb
@@ -0,0 +1,16 @@
+name = 'Jamie Fletcher'
+age = 41 # actually true
+height = 183 # cm
+weight = 80 # kg
+eyes = 'Green-ish'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}"
+puts "He's #{height} cm tall"
+puts "He's #{weight} kg heavy"
+puts "Not too heavy"
+puts "He's got #{eyes} eyes and #{hair} hair"
+puts "His teeth are usually #{teeth} depening on the coffee"
+
+puts "If I add #{age}, #{height} and #{weight} I get #{age + height + weight}."
