d08a00a4fb6ecf24663bd3be365c77040b23e58d
jamiemfletcher@gmail.com <jamiemfletcher@gmail.com>
2016-06-21 17:06:39 +0100

    Add Exercise 9

---
+++ ex9.rb
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+puts "Here are the days: #{days}"
+puts "Here are the months: #{months}"
+
+puts """
+ruby heredoc
+ruby heredoc
+ruby heredoc
+ruby heredoc
+"""
